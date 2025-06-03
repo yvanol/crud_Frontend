6fd3b3a67ecf5ce40bb35ce8fea1b849ea918d59
yvanol <rikamyvanol@gmail.com>
2025-06-03 22:22:25 +0200

    update login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios'; // Added missing import
+import axios from 'axios'; 
 
 function Login({ setIsAuthenticated }) {
   const [name, setName] = useState('');
